855b36e50ca5183633b28b1f6571a1639acd5119
sortupdev5 <sortup.dev5@gmail.com>
2026-02-25 16:09:32 +0500

    reload issue fixed

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -11,11 +11,11 @@ api.interceptors.response.use(
     async (error) => {
         const originalRequest = error.config;
 
-        // If error is 401 and message is "Access token expired" and we haven't retried yet
+        // If error is 401 and (token is expired OR token is missing)
         if (
             error.response &&
             error.response.status === 401 &&
-            error.response.data.message === "Access token expired" &&
+            (error.response.data.message === "Access token expired" || error.response.data.message === "No Token Provided") &&
             !originalRequest._retry
         ) {
             originalRequest._retry = true;
